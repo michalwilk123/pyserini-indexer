e970aa749515b1af3c0a5363a398114a7e9d284c
michal <michalwilk139@gmail.com>
2023-05-28 16:21:09 +0200

    create searcher tests

--- handlers/searcher.py
+++ handlers/searcher.py
@@ -14,27 +14,32 @@ def get_searcher(
     *,
     dense_model: Optional[str] = None,
     sparse_model: Optional[str] = None,
+    second_index_dir: Optional[str] = None,
 ):
     dense_searcher = None
     sparse_searcher = None
 
-    if dense_model == models.DenseIndexes.ANCE.value:
+    if dense_model == models.DenseIndexes.ANCE.name:
         encoder = faiss.AnceQueryEncoder(models.ANCE_MODEL)
         dense_searcher = faiss.FaissSearcher(index_dir, encoder)
-    elif dense_model == models.DenseIndexes.TCT_COLBERT.value:
-        encoder = faiss.TctColBertQueryEncoder(models.DenseIndexes.TCT_COLBERT)
+    elif dense_model == models.DenseIndexes.TCT_COLBERT.name:
+        encoder = faiss.TctColBertQueryEncoder(models.TCT_BERT_MODEL)
         dense_searcher = faiss.FaissSearcher(index_dir, encoder)
-    elif dense_model == models.DenseIndexes.MINILM_V2.value:
+    elif dense_model == models.DenseIndexes.MINILM_V2.name:
         encoder = faiss.AutoQueryEncoder(models.AUTO_MODEL)
         dense_searcher = faiss.FaissSearcher(index_dir, encoder)
 
-    if sparse_model == models.SparseIndexes.BM25.value:
-        sparse_searcher = lucene.LuceneSearcher(index_dir)
-    elif sparse_model == models.SparseIndexes.UNICOIL.value:
-        sparse_searcher = lucene.LuceneImpactSearcher(index_dir, models.UNICOIL_MODEL)
+    sparse_index_dir = second_index_dir or index_dir
+
+    if sparse_model == models.SparseIndexes.BM25.name:
+        sparse_searcher = lucene.LuceneSearcher(sparse_index_dir)
+    elif sparse_model == models.SparseIndexes.UNICOIL.name:
+        sparse_searcher = lucene.LuceneImpactSearcher(
+            sparse_index_dir, models.UNICOIL_MODEL
+        )
 
     if sparse_searcher and dense_searcher:
-        return hybrid.HybridSearcher(dense_searcher, sparse_model)
+        return hybrid.HybridSearcher(dense_searcher, sparse_searcher)
     elif sparse_searcher:
         return sparse_searcher
     elif dense_searcher:
